fd9817f00db99709c25b4a6fefa837653f66067a
Mohammad Alizadeh <mhalizadeh@hotmail.com>
2023-07-30 18:57:49 +0200

    Add string case reversal functionality

--- CSharpPracticeExercises/ReverseStringCasing/Program.cs
+++ CSharpPracticeExercises/ReverseStringCasing/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Text;
 
 namespace ReverseStringCasing
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            
+            var input = GetInputString();
+            var reversedCase = ReverseStringCasing(input);
+            Console.WriteLine($"Reversed case: {reversedCase}");
+        }
+
+        private static string ReverseStringCasing(string inputText)
+        {
+            StringBuilder reversedCase = new();
+            foreach (var character in inputText)
+            {
+                if (!char.IsLetter(character))
+                    reversedCase.Append(character);
+                else
+                    reversedCase.Append(char.IsLower(character) ? char.ToUpper(character) : char.ToLower(character));
+            }
+
+            return reversedCase.ToString();
+        }
+
+        private static string GetInputString()
+        {
+            Console.WriteLine("Enter a Text:");
+            return Console.ReadLine();
         }
     }
 }
